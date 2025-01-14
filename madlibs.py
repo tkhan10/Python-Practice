5269ae5c3ce054075d1e3a75cee7613b405bd209
tofekkhan <tofeequekhan87@gmail.com>
2025-01-13 23:41:39 -0600

    madlibs program.

---
+++ madlibs.py
@@ -0,0 +1,17 @@
+# This is madlibs game in pythod \
+# Tofek created this program for learning purpose.
+
+youtuber = "Tofek Khan" # This is some you tuber name;
+
+# print("Subscribe to "+youtuber);
+# print("Subscribe to {}".format(youtuber));
+# print(f"Subscribe to {youtuber}");
+
+weather = input("weather -")
+place = input("place")
+king = input("king")
+
+madlibs = f"Once upon time, long time ago when all {weather} in all over the world, there are so many animals in {place} \
+and there was a {king}, who was a king of {place}"
+
+print(madlibs)
