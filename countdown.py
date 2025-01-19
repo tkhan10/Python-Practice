a7767df13108f09b58f1d770ee321f392beac7fa
tofekkhan <tofeequekhan87@gmail.com>
2025-01-19 00:56:39 -0600

    countdown

---
+++ countdown.py
@@ -0,0 +1,16 @@
+import time
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t = t-1
+
+    print("Timer Completed!")
+
+
+input = input("Enter the time : ")
+
+countdown(int(input))
